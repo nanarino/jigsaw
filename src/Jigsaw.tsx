865961bc844d550217e06fca8e9f89e58a7d2feb
☘ <kogawananari@gmail.com>
2023-03-17 23:19:39 +0800

    feat

--- src/Jigsaw.tsx
+++ src/Jigsaw.tsx
@@ -1,10 +1,11 @@
-import { Component, createSignal, Index } from 'solid-js';
+import { Component, createSignal, Index, createEffect, untrack, on } from 'solid-js';
 import './Jigsaw.scss';
 import Alert from './Alert';
 import { state, setState } from './store';
+import type { pst } from './tools';
+import { shuffle, isValid } from './tools';
 
 
-type pst = `${0 | 1 | 2 | 3}${0 | 1 | 2 | 3}`
 const LAST = 15
 const getInitPstArray = () => (new Array(LAST + 1).fill(0)).map((i, j) => ('0' + j.toString(4)).slice(-2)) as pst[]
 const orgArray: pst[] = getInitPstArray();
@@ -19,6 +20,36 @@ const Jigsaw: Component = () => {
   setState({ width });
   const [getPstArray, setPstArray] = createSignal<pst[]>(getInitPstArray(), { equals: false });
 
+
+  // Start
+  const [getStep, setStep] = createSignal(-2);
+  createEffect(() => {
+    if (getStep() === -1) { // when step: -1 -> 0
+      openSign[1](true);
+    }
+  })
+  createEffect(on(openSign[0], () => {
+    if (openSign[0]() === false) { // when step: true -> false
+      do {
+        setPstArray(shuffle)
+      } while (!isValid(getPstArray()));
+      setStep(0)
+    }
+  }, { defer: true }))
+
+
+  // Win
+  createEffect(() => {
+    if (getStep() > 0) {
+      for (const [index, thisPst] of Object.entries(untrack(getPstArray))) {
+        if (thisPst !== orgArray[index]) return
+      }
+      setMessage('All Clear');
+      openSign[1](true);
+    }
+  })
+
+
   return (
     <div id="jigsawview" style={{
       width: (4 * width + 2) + 'px',
@@ -31,17 +62,17 @@ const Jigsaw: Component = () => {
               width: (width - 2) + 'px',
               height: (width - 2) + 'px',
               top: (width * (+getPst()[0])) + 'px',
-              left: (width * (+getPst()[1])) + 'px'
+              left: (width * (+getPst()[1])) + 'px',
+              "pointer-events": (openSign[0]() ? "none" : "auto")
             }} onClick={
               () => {
                 const lastPstNum = +getPstArray()[LAST];
-                const thisPstNum = +getPstArray()[THIS]; 
+                const thisPstNum = +getPstArray()[THIS];
                 if (LAST !== THIS && [10, -10, 1, -1].includes(lastPstNum - thisPstNum)) {
                   setPstArray(now => (
                     [now[LAST], now[THIS]] = [now[THIS], now[LAST]], now
                   ))
-                } else {
-                  
+                  setStep(step => ++step);
                 }
               }
             }>
---
+++ src/tools.ts
@@ -0,0 +1,24 @@
+export type pst = `${0 | 1 | 2 | 3}${0 | 1 | 2 | 3}`
+
+export function shuffle<T>(arr: T[]): T[] { //打乱数组
+    const len = arr.length;
+    for (let i = 0; i < len - 1; i++) {
+        let idx = Math.floor(Math.random() * (len - i));
+        let temp = arr[idx];
+        arr[idx] = arr[len - i - 1];
+        arr[len - i - 1] = temp;
+    }
+    return arr;
+};
+
+export const isValid = (arr: pst[]) => { //判断数列的逆序是否为偶数  偶数=>有解
+    let count = 0, len = arr.length;
+    for (let i = 0; i < len; i++) {
+        for (let j = i + 1; j < len; j++) {
+            if (arr[j] < arr[i]) {
+                count++;
+            }
+        }
+    }
+    return count % 2 === 0;
+};
